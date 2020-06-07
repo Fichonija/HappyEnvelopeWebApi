981bf794ccdc3f432c401b68c050d88fb760f5c0
Fichonija <bfilip9205@gmail.com>
2020-06-07 19:35:18 +0200

    Add project files.

---
+++ HappyEnvelopeWebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HappyEnvelopeWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ HappyEnvelopeWebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HappyEnvelopeWebApi.WebApiApplication" Language="C#" %>
